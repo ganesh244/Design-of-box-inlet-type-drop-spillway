7823fc9fa49996fce14f007a792c2734ae0eaccb
Ganeshkeesara <69961062+ganesh244@users.noreply.github.com>
2024-02-24 12:43:28 +0530

    Update box inlet.js

--- box inlet.js
+++ box inlet.js
@@ -11,18 +11,22 @@ document.getElementById("spillwayForm").addEventListener("submit", function(even
     let x2 = Math.pow(x1, 2);
     let x3 = x2 / g;
     let Dc = Math.sqrt(x3);
+    let DcUnits = "m";
 
     // critical depth at end sill
     let y1 = Qg / We;
     let y2 = Math.pow(y1, 2);
     let y3 = y2 / g;
     let Dce = Math.sqrt(y3);
+    let DceUnits = "m";
 
     // Length of straight section
     let Ls = ((0.2 / (B / W)) + 1) * Dc;
+    let LsUnits = "m";
 
     // Length of basin section
     let Lb = ((W + 2 * B) / (2 * B / W));
+    let LbUnits = "m";
 
     // Tail water depth
     let D2;
@@ -31,9 +35,11 @@ document.getElementById("spillwayForm").addEventListener("submit", function(even
     } else {
         D2 = 1.6 * Dce;
     }
+    let D2Units = "m";
 
     // Height of end sill and longitudinal sill
     let F = D2 / 6;
+    let FUnits = "m";
 
     // Number of longitudinal sills
     let N = We < 2.5 * W ? 2 : 4;
@@ -41,19 +47,21 @@ document.getElementById("spillwayForm").addEventListener("submit", function(even
     // Space between longitudinal sills
     let P1 = W / 6;
     let P2 = W / 4;
+    let PUnits = "m";
 
     // Height of side wall above tail water Depth
     let t = D2 / 3;
+    let tUnits = "m";
 
-    document.getElementById("Dc").innerText = Dc.toFixed(16);
-    document.getElementById("Dce").innerText = Dce.toFixed(16);
-    document.getElementById("Ls").innerText = Ls.toFixed(16);
-    document.getElementById("Lb").innerText = Lb.toFixed(16);
-    document.getElementById("d2").innerText = D2.toFixed(16);
-    document.getElementById("F").innerText = F.toFixed(16);
+    document.getElementById("Dc").innerText = Dc.toFixed(2) + " " + DcUnits;
+    document.getElementById("Dce").innerText = Dce.toFixed(2) + " " + DceUnits;
+    document.getElementById("Ls").innerText = Ls.toFixed(2) + " " + LsUnits;
+    document.getElementById("Lb").innerText = Lb.toFixed(2) + " " + LbUnits;
+    document.getElementById("d2").innerText = D2.toFixed(2) + " " + D2Units;
+    document.getElementById("F").innerText = F.toFixed(2) + " " + FUnits;
     document.getElementById("numSills").innerText = N;
-    document.getElementById("P").innerText = P1.toFixed(16) + " to " + P2.toFixed(16);
-    document.getElementById("t").innerText = t.toFixed(16);
+    document.getElementById("P").innerText = P1.toFixed(2) + " to " + P2.toFixed(2) + " " + PUnits;
+    document.getElementById("t").innerText = t.toFixed(2) + " " + tUnits;
 
     document.getElementById("result").style.display = "block";
 });
